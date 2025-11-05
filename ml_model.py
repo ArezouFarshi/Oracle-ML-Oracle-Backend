b9008c25f6389f5747edae9eb63f188ef1d8872d
ArezouFarshi <arezou.farshi@mail.polimi.it>
2025-11-05 21:13:51 +0100

    Update ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -3,23 +3,23 @@ import numpy as np
 import os
 
 MODEL_PATH = "fault_model.pkl"
-model = None
+
+def is_model_available():
+    return os.path.exists(MODEL_PATH)
 
 def load_model():
-    global model
-    if os.path.exists(MODEL_PATH):
-        model = joblib.load(MODEL_PATH)
-    else:
-        model = None
+    if is_model_available():
+        return joblib.load(MODEL_PATH)
+    return None
 
 def predict_fault(data: dict):
-    if model is None:
+    if not is_model_available():
         return False, {"error": "Model not trained yet."}
     try:
+        model = load_model()
         features = np.array([
-            data["temperature"],
-            data["humidity"],
-            data["tilt"]
+            data["surface_temp"],
+            data["ambient_temp"]
         ]).reshape(1, -1)
         prediction = model.predict(features)[0]
         return True, {"prediction": int(prediction)}
