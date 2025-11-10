27ecf603f1e221835320205d30b429d9529d68fc
ArezouFarshi <arezou.farshi@mail.polimi.it>
2025-11-10 21:53:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -272,4 +272,4 @@ def ingest():
 
 # --- Entrypoint ---
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 1000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
